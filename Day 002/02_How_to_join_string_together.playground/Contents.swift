b278e6239e7a6339f3e60c690f44ba9b22f59bc6
Glenn Hevey <glenn.hevey@gmail.com>
2023-06-07 20:11:11 +1000

    Day 002 completed

---
+++ Day 002/02_How_to_join_string_together.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+
+let lyric = people + " gonna " + action
+
+let name = "Taylor"
+let age = 26
+let message = "hello, my name is \(name) and I am \(age) years old"
---
+++ Day 002/Checkpoint_01.playground/Contents.swift
@@ -0,0 +1,5 @@
+import Cocoa
+
+let celsius = 26.0
+let fahrenheit = celsius * 9 / 5 + 32
+print("\(celsius)° celsius is \(fahrenheit)° fahrenheit")

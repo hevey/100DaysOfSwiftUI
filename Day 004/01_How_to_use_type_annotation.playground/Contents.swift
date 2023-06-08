d145e172fd01d48e6567b05ebec182ab88de25a2
Glenn Hevey <glenn.hevey@gmail.com>
2023-06-09 08:09:34 +1000

    Day 004 completed

---
+++ Day 004/01_How_to_use_type_annotation.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+let surname: String = "Lasso"
+var score: Double = 0
+
+var teams: [String] = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style = UIStyle.light
+style = .dark
+
+let userName: String
+
+userName = "@hevey"
+
+print(userName)
---
+++ Day 004/Checkpoint_02.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+let cities = ["Hobart", "Melbourne", "Sydney", "Perth", "Perth"]
+
+print("Amount of cities: \(cities.count)")
+
+let citiesSet = Set(cities)
+
+print("Unique amount of cities: \(citiesSet.count)")

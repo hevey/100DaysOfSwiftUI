2c420a70f6a6d62e2b8a817100212eef3d28aad3
Glenn Hevey <glenn.hevey@gmail.com>
2023-06-28 22:18:52 +1000

    Project 05 completed

---
+++ Project 05/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Glenn Hevey on 28/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

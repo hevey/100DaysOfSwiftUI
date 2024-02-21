4bda880541a58d43bddf7e0c2df1d7dd3e5ec603
Glenn Hevey <glenn.hevey@gmail.com>
2024-02-21 17:41:14 +1100

    challenge Day 03

---
+++ Challenge Day 03/Challenge Day 03/Challenge_Day_03App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_Day_03App.swift
+//  Challenge Day 03
+//
+//  Created by Glenn Hevey on 19/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_Day_03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge Day 03/Challenge Day 03/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge Day 03
+//
+//  Created by Glenn Hevey on 19/7/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!!!!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
